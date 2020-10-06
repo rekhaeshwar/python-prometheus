d2626a05656ca1aecb8389cafd57056e0d13067b
Rekha Eshwarappa <reshwarappa@dfcorp.drfirst.com>
2020-10-06 15:12:08 -0400

    added timeout as a new config and code to implement it

--- config/config.py
+++ config/config.py
@@ -1,1 +1,2 @@
 urls=["https://httpstat.us/503", "https://httpstat.us/200"]
+request_timeout_in_seconds=30
--- main.py
+++ main.py
@@ -12,6 +12,8 @@ metrics = {}
 metrics['response_time']=Gauge('sample_external_url_response_ms', 'Response Time of the url',['url'])
 metrics['status']=Gauge('sample_external_url_up', 'Status of the url',['url'])
 
+REQUEST_TIMEOUT_SECONDS=cfg.request_timeout_in_seconds
+
 @app.route('/queryurl')
 def checkurl():
     logging.info("Query url function.")
@@ -23,26 +25,37 @@ def checkurl():
     for url in urls:
         try:
             logging.info("Querying url "+ url )
-            response=requests.get(url)
+            response=requests.get(url,timeout=REQUEST_TIMEOUT_SECONDS)
+            response_time_in_ms=response.elapsed.total_seconds()*1000
             if response.ok:
                 logging.info( url + " url responded successfully.")
                 response_hash['response'][url]={}
                 response_hash['response'][url]['status_code']=response.status_code
                 response_hash['response'][url]['up']=1
-                response_hash['response'][url]['response_time_in_ms']=response.elapsed.total_seconds()*1000
+                response_hash['response'][url]['response_time_in_ms']=response_time_in_ms
                 metrics['status'].labels(url).set(1)
-                metrics['response_time'].labels(url).set(response.elapsed.total_seconds()*1000)
+                metrics['response_time'].labels(url).set(response_time_in_ms)
             else:
                 logging.info( url + " responded with unhealthy response code.")
                 response_hash['response'][url]={}
                 response_hash['response'][url]['status_code']=response.status_code
                 response_hash['response'][url]['up']=0
-                response_hash['response'][url]['response_time_in_ms']=response.elapsed.total_seconds()*1000
+                response_hash['response'][url]['response_time_in_ms']=response_time_in_ms
                 metrics['status'].labels(url).set(0)
-                metrics['response_time'].labels(url).set(response.elapsed.total_seconds()*1000)
+                metrics['response_time'].labels(url).set(response_time_in_ms)
+        except requests.Timeout as ex:
+            logging.warning("Request timedout after " + str(REQUEST_TIMEOUT_SECONDS)+" seconds!")
+            response_time_in_ms=REQUEST_TIMEOUT_SECONDS*1000
+            response_hash['response'][url]={}
+            response_hash['response'][url]['status_code']=503
+            response_hash['response'][url]['up']=0
+            response_hash['response'][url]['response_time_in_ms']=response_time_in_ms
+            metrics['status'].labels(url).set(0)
+            metrics['response_time'].labels(url).set(response_time_in_ms)
         except Exception as ex:
             logging.warning("Something went wrong!")
             logging.warning(ex)
+
     response_json=json.dumps(response_hash)
     return json.loads(response_json)
 
