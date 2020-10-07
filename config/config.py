55350c3ef3664b2cc6b8008470e2ea80a848d97b
Rekha Eshwarappa <reshwarappa@dfcorp.drfirst.com>
2020-10-06 23:31:27 -0400

    code changes

--- config/config.py
+++ config/config.py
@@ -1,2 +1,4 @@
 urls=["https://httpstat.us/503", "https://httpstat.us/200"]
 request_timeout_in_seconds=30
+query_interval_in_seconds=300
+log_level="INFO"
--- main.py
+++ main.py
@@ -4,18 +4,26 @@ from prometheus_client import start_http_server, Histogram,  generate_latest,Sum
 import config.config as cfg
 import logging
 import json
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+
+# setting log level as per config/config.py
+log_level=logging.getLevelName(cfg.log_level)
+logging.basicConfig(format='%(asctime)s - %(message)s',level=log_level)
 
 app = Flask(__name__)
 
-## metrics for Prometheus
+# metrics for Prometheus
 metrics = {}
 metrics['response_time']=Gauge('sample_external_url_response_ms', 'Response Time of the url',['url'])
 metrics['status']=Gauge('sample_external_url_up', 'Status of the url',['url'])
 
+# reading config from config/config.py
 REQUEST_TIMEOUT_SECONDS=cfg.request_timeout_in_seconds
+QUERY_INTERVAL_IN_SECONDS=cfg.query_interval_in_seconds
 
 def construct_url_response(url,status_code,status_up,response_time_in_ms):
-    logging.info("constructing response per url" )
+    logging.info("constructing response for url - " +url)
     response_hash={}
     response_hash['status_code']=status_code
     response_hash['up']=status_up
@@ -24,11 +32,12 @@ def construct_url_response(url,status_code,status_up,response_time_in_ms):
     metrics['response_time'].labels(url).set(response_time_in_ms)
     return response_hash
 
+# endpoint to run url query
 @app.route('/queryurl')
 def checkurl():
-    logging.info("Query url function.")
+    logging.info("Query url function started.")
 
-    ## reading config from config/config.py
+    # reading config from config/config.py
     urls=cfg.urls
     result={}
     result['response']={}
@@ -44,14 +53,15 @@ def checkurl():
                 logging.info( url + " responded with unhealthy response code.")
                 result['response'][url]=construct_url_response(url,response.status_code,0,response_time_in_ms)
         except requests.Timeout as ex:
-            logging.warning("Request timedout after " + str(REQUEST_TIMEOUT_SECONDS)+" seconds!")
+            logging.warning("Request timed out after " + str(REQUEST_TIMEOUT_SECONDS)+" seconds for url - "+ url)
             response_time_in_ms=REQUEST_TIMEOUT_SECONDS*1000
             result['response'][url]=construct_url_response(url,503,0,response_time_in_ms)
         except Exception as ex:
             logging.warning("Something went wrong!")
-            logging.warning(ex)
+            logging.exception(ex)
             result['response'][url]={"message":"Exception occured"}
     result['status']='ok'
+    logging.info("Query url function completed.")
     return json.dumps(result)
 
 @app.route("/metrics")
@@ -62,11 +72,20 @@ def requests_count():
         res.append(generate_latest(v))
     return Response(res, mimetype="text/plain")
 
+# endpoint to monitor health of the application
 @app.route('/healthcheck')
 def healthcheck():
     logging.info("healthcheck function.")
     return_json={"app_name":"query_url","app_status":"ok"}
     return return_json
+
+# schedule running the query as a background job
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=checkurl, trigger="interval", seconds=QUERY_INTERVAL_IN_SECONDS)
+scheduler.start()
+
+# shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
