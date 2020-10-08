face1dc54d44920054e3b2ed238e143f9e2f3735
Rekha Eshwarappa <reshwarappa@dfcorp.drfirst.com>
2020-10-07 23:56:10 -0400

    docker credentials

--- deploy.sh
+++ deploy.sh
@@ -11,3 +11,21 @@ kubectl apply -f kubernetes/deploy.yaml
 
 #creates service to expose the deployment
 kubectl apply -f kubernetes/service.yaml
+
+counter=1
+while [ $counter -le 3 ]
+do
+  if [[ "$(kubectl rollout status deployment/query-url -n query-url)" =~ .*"successfully rolled out".* ]]; then
+    echo "deployment is successfully rolled out"
+    break
+  else
+    if [[ $counter -eq 3 ]]; then
+     echo "deployment failed" 
+     exit 1
+    else
+     echo "waiting for deployment rollout"
+     sleep 10s
+   fi
+  fi
+  ((counter++))
+done
